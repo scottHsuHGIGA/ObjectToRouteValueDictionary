0884aa09efaaaeea21ef9aeda6e9cf6f6a84572c
scott <scott.hsu@hgiga.com>
2021-09-03 09:01:16 +0800

    second commit

---
+++ Library/Helper/UrlHelper.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Routing;
+
+namespace Library.Helper
+{
+    public static class UrlHelper
+    {
+        /// <summary>
+        /// 物件轉換成RouteValueDictionary
+        /// </summary>
+        /// <typeparam name="T">泛型</typeparam>
+        /// <param name="obj">物件</param>
+        /// <returns></returns>
+        public static RouteValueDictionary ObjectToRouteValueDictionary<T>(T obj)
+        {
+            var rvDic = new RouteValueDictionary();
+
+            foreach (var property in obj.GetType().GetProperties())
+            {
+                //type符合集合類型(Array暫時沒有)
+                if ((property.PropertyType.IsGenericType && property.PropertyType.GetGenericTypeDefinition() == typeof(IEnumerable<>)) ||
+                       typeof(ICollection).IsAssignableFrom(property.PropertyType))
+                {
+                    IEnumerable t = (IEnumerable)property.GetValue(obj);
+                    if (t != null)
+                    {
+                        var i = 0;
+                        foreach (var item in t)
+                        {
+                            rvDic.Add(string.Concat(property.Name, "[", i, "]"), item);
+                            i++;
+                        }
+                    }
+
+                }
+                else
+                {
+                    var value = property.GetGetMethod().Invoke(obj, null) == null ? "" : property.GetGetMethod().Invoke(obj, null);
+                    if (!string.IsNullOrEmpty(value.ToString()))
+                    {
+                        rvDic.Add(property.Name, value);
+                    }
+
+                }
+            }
+            return rvDic;
+        }
+    }
+}
---
+++ ObjectToRouteValueDictionary/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ObjectToRouteValueDictionary.Controllers
+{
+    public class HomeController : Controller
+    {
+        public class TestModel
+        {
+            public string Astring { get; set; }
+            public string Bstring { get; set; }
+            public string Cstring { get; set; }
+        }
+
+        public ActionResult Index()
+        {
+            var model = new TestModel()
+            {
+                Astring = "這是A",
+                Bstring = "這是B",
+                Cstring = "這是C"
+            };
+            ViewData["TestModel"] = model;
+            ViewData["ActionName"] = "Index";
+            return View();
+        }
+
+        public ActionResult Test(TestModel model)
+        {
+            ViewData["TestModel"] = model;
+            ViewData["ActionName"] = "Test";
+            return View("Index");
+        }
+
+    }
+}
